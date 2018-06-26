f63b0c2da11f873e094758ed9dd63c49dd023465
Nurzhamal <37982054+Nurzamal@users.noreply.github.com>
2018-06-26 16:05:20 +0600

    Update and rename Main.js to TodoList.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class TodoList extends React.Component{
+   
+    render(){
+        const todos = this.props.todos
+        return(
+        <div>
+            <ul className="list-group">
+                {todos.map((todo) =>
+                    <li key={todo.task_id} className="list-group-item">
+                        {todo.description}
+                        <button type="button" className="close right" aria-label="Close"
+                                onClick={() => this.props.deleteTodo(todo.task_id)}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </li>
+                )}
+            </ul>
+        </div>
+        )
+    }
+}
