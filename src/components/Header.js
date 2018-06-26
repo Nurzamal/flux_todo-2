3abc75c786fdafef1661c9b6b885809d74e7536a
Nurzhamal <37982054+Nurzamal@users.noreply.github.com>
2018-06-26 12:23:15 +0600

    transferred from the function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
-export function Header(props) {
-    let input
+class Header extends React.Component {
+  state = {
+    input: ''
+  }
+
+  onChange = (e) => {
+    this.setState({ input: e.target.value })
+  }
+
+  onSubmit = (e) => {
+    this.props.submit(this.state.input)
+  }
+
+  render() {
     return (
-        <header id="header">
-            <h1>To Do list</h1>
-            <form onSubmit={function(e){
-                e.preventDefault()
-                props.onCreateTodo(input.value)
-                input.value = ''
-            }}>
-                <input
-                    ref={node => {
-                        input = node
-                    }}
-                />
-                <button type="submit"> Add Todo </button>
-            </form>
-        </header>
-    );
-}
+      <header id="header">
+      <h1>To Do list</h1>
+      <form>
+        <input onChange={this.onChange} />
+         <button id="formSubmit" name="submitButton" onClick={this.onSubmit}> Add Todo</button>
+      </form>
+    )
+  }
+   
