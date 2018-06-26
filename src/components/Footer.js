2bcafe34184d53e6be5f0aba413ee786ec834b2b
Nurzhamal <37982054+Nurzamal@users.noreply.github.com>
2018-06-26 17:05:06 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,26 +14,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-      <ul>
-      {todos.map(todo =>
-        <div key={todo.id}>
-          <Todo todo={todo} onClick={onTodoClick}/>
-          <button onClick={()=>{onDeleteTodo(todo.id)}}>delete</button>
-        </div>
-      )}
-    </ul>
         <button id="formSubmit" className="btn-all" name="submitButton" onClick={this.handleClick}> Get todos from API</button>
       </div>
     );
-TodoList.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    completed: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired
-  }).isRequired).isRequired,
-  onTodoClick: PropTypes.func.isRequired
-}
-
-export default TodoList
   }
 }
