b40f48d72c7e3b513cd4901d3976b5c7ff3974ff
Nurzhamal <37982054+Nurzamal@users.noreply.github.com>
2018-06-27 14:03:34 +0600

    using componentDidMount()

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,14 +13,15 @@ export default class Footer extends React.Component {
       this.props.onGetTodos();
     }
 
+    componentDidMount() {
+      this.props.onGetTodos();
+    }
+
     render() {
         const phrase = this.state.remaining === 1 ? ' more job left' : ' more jobs left';
 
         return (
             <footer id="footer">
-              <div>
-                <button id="btn-all" onClick={this.handleEvent}>Get Todos from Api</button>
-              </div>
               <div id="items">
                 <strong> {this.state.remaining} </strong> {phrase}
               </div>
